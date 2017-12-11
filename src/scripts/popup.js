c1234603d0edcfc1862387d9687c322086a6d127
Dave Loyall <dave@the-good-guys.net>
2017-12-11 17:46:55 -0600

    blindly making changes from eslint complaints
    part 11: remove tabs

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,18 +1,18 @@
 function save_now_function(){
-	var wb_url = "https://web.archive.org/save/";
-	chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"save" }).then(handleResponse, handleError);	
+  var wb_url = "https://web.archive.org/save/";
+  chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"save" }).then(handleResponse, handleError);	
 }
 
 function recent_capture_function(){
-	var wb_url = "https://web.archive.org/web/2/";
-	chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"recent" }, function(response) {
-	});
+  var wb_url = "https://web.archive.org/web/2/";
+  chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"recent" }, function(response) {
+  });
 }
 
 function first_capture_function(){
-	var wb_url = "https://web.archive.org/web/0/";
-	chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"first" }, function(response) {
-	});
+  var wb_url = "https://web.archive.org/web/0/";
+  chrome.runtime.sendMessage({message: "openurl", wayback_url: wb_url, method:"first" }, function(response) {
+  });
 }
 
 document.getElementById("save_now").onclick = save_now_function;
@@ -21,6 +21,6 @@ document.getElementById("first_capture").onclick = first_capture_function;
 
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   if(message.message==="urlnotfound"){
-  	alert("URL not found in wayback archives!");
+    alert("URL not found in wayback archives!");
   }
 });
